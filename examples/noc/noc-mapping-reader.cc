fa44ec706f5683bd3ee70a167fbe0329ccea06ab
cipi <cipi@localhost>
2010-08-11 16:55:28 +0300

    the noc-mapping-reader is now able to get all the information from the XMLs

--- examples/noc/noc-mapping-reader.cc
+++ examples/noc/noc-mapping-reader.cc
@@ -6,26 +6,209 @@
  */
 
 #include <iostream>
+
 #include "../../../CTG-XML/src/ro/ulbsibiu/acaps/ctg/xml/mapping/mapping.hxx"
+#include "../../../CTG-XML/src/ro/ulbsibiu/acaps/ctg/xml/apcg/apcg.hxx"
+#include "../../../CTG-XML/src/ro/ulbsibiu/acaps/ctg/xml/ctg/ctg.hxx"
+#include "../../../CTG-XML/src/ro/ulbsibiu/acaps/ctg/xml/core/core.hxx"
+#include "../../../CTG-XML/src/ro/ulbsibiu/acaps/ctg/xml/task/task.hxx"
+
+#include "ns3/assert.h"
 
 using namespace std;
+
 using namespace mapping;
+using namespace apcg;
+using namespace ctg;
+using namespace core;
+using namespace task;
+
 using xml_schema::flags;
 
+const char FILE_SEPARATOR = '/';
+
+apcgType::core_type
+findCoreInApcg(apcgType theApcgType, string coreId)
+{
+  apcgType::core_type theApcgCoreType("");
+  for (apcgType::core_iterator i(theApcgType.core().begin()); i
+      != theApcgType.core().end(); i++)
+    {
+      if ((*i).id() == coreId)
+        {
+          theApcgCoreType = *i;
+          break;
+        }
+    }
+  return theApcgCoreType;
+}
+
 int
-main (int argc, char* argv[])
+main(int argc, char* argv[])
 {
   try
-  {
-    const string xmlFile("../Mapper/xml/e3s/auto-indust-mocsyn.tgff/ctg-0/mapping-0.xml");
-    auto_ptr<mappingType> mt (mapping::mapping (xmlFile, flags::dont_validate));
+    {
+      // parse the mapping XML
 
-    cerr << mt->id() << endl;
+      string mappingXmlFilePath(
+          "../Mapper/xml/e3s/auto-indust-mocsyn.tgff/ctg-0/mapping-0.xml");
 
-  }
+      auto_ptr<mappingType> theMappingType(mapping::mapping(mappingXmlFilePath,
+          flags::dont_validate));
+
+      cerr << "Mapping ID: " << theMappingType->id() << " APCG ID: "
+          << theMappingType->apcg() << endl;
+
+      //      for (mappingType::map_const_iterator i(theMappingType->map().begin()); i
+      //          != theMappingType->map().end(); i++)
+      //        {
+      //          mapType theMapType = *i;
+      //          cerr << "\t node: " << theMapType.node() << endl;
+      //          cerr << "\t core: " << theMapType.core() << endl;
+      //          cerr << endl;
+      //        }
+
+      string ctgFilePath = mappingXmlFilePath.substr(0,
+          mappingXmlFilePath.find_last_of(FILE_SEPARATOR));
+      //      cerr << "CTG file path: " << ctgFilePath << endl;
+
+      string apcgXmlFilePath = ctgFilePath + FILE_SEPARATOR + "apcg-"
+          + theMappingType->apcg() + ".xml";
+      //      cerr << "APCG XML file path: " << apcgXmlFilePath << endl;
+      //      cerr << endl;
+
+      // parse the APCG XML
+
+      auto_ptr<apcgType> theApcgType(apcg::apcg(apcgXmlFilePath,
+          flags::dont_validate));
+
+      NS_ASSERT_MSG (theMappingType->apcg() == theApcgType->id(),
+          "Mapping XML says the APCG ID is " << theMappingType->apcg()
+          << " but, APCG XML says the APCG ID is " << theApcgType->id() << "!");
+
+      cerr << "APCG ID: " << theApcgType->id() << " CTG ID: "
+          << theApcgType->ctg() << endl;
+
+      //      for (apcgType::core_const_iterator i(theApcgType->core().begin()); i
+      //          != theApcgType->core().end(); i++)
+      //        {
+      //          apcgType::core_type theApcgCoreType = *i;
+      //          cerr << "\t core: " << theApcgCoreType.id() << endl;
+      //          for (apcgType::core_type::task_const_iterator i(
+      //              theApcgCoreType.task().begin()); i != theApcgCoreType.task().end(); i++)
+      //            {
+      //              apcgType::core_type::task_type theTaskType = *i;
+      //              cerr << "\t\t task: " << theTaskType << endl;
+      //              cerr << endl;
+      //            }
+      //        }
+
+      string ctgXmlFilePath = ctgFilePath + FILE_SEPARATOR + "ctg-"
+          + theApcgType->ctg() + ".xml";
+      //      cerr << "CTG XML file path: " << ctgXmlFilePath << endl;
+      //      cerr << endl;
+
+      // parse the CTG XML
+
+      auto_ptr<ctgType> theCtgType(ctg::ctg(ctgXmlFilePath,
+          flags::dont_validate));
+
+      NS_ASSERT_MSG (theApcgType->ctg() == theCtgType->id(),
+          "APCG XML says the CTG ID is " << theApcgType->ctg()
+          << " but, CTG XML says the CTG ID is " << theCtgType->id() << "!");
+
+      cerr << "CTG ID: " << theCtgType->id() << " CTG period: "
+          << theCtgType->period() << endl;
+
+      // extract all required information from all XMLs
+
+      string coresFilePath = ctgFilePath.substr(0, ctgFilePath.find_last_of(
+          FILE_SEPARATOR) + 1) + "cores";
+
+      string tasksFilePath = ctgFilePath + FILE_SEPARATOR + "tasks";
+
+      for (mappingType::map_const_iterator i(theMappingType->map().begin()); i
+          != theMappingType->map().end(); i++)
+        {
+          mapType theMapType = *i;
+          cerr << "\t node: " << theMapType.node() << endl;
+          cerr << "\t core: " << theMapType.core();
+
+          string coreXmlFilePath = coresFilePath + FILE_SEPARATOR + "core-"
+              + theMapType.core() + ".xml";
+
+          auto_ptr<core::coreType> theCoreType(core::core(coreXmlFilePath,
+              flags::dont_validate));
+          NS_ASSERT_MSG (theMapType.core() == theCoreType->ID(), "APCG XML says the core ID is " << theMapType.core()
+              << " but, core XML says the ID is " << theCoreType->ID() << "!");
+          cerr << " (name: " << theCoreType->name() << " frequency: "
+              << theCoreType->frequency() << " height: "
+              << theCoreType->height() << " width: " << theCoreType->width()
+              << " idle power: " << theCoreType->idlePower() << ")" << endl;
+
+          // Note that I am dereferencing theApcgType. If I am working with the (auto) pointer,
+          // at the second iteration theApcgType becomes NULL and I don't know why...
+          apcgType::core_type theApcgCoreType = findCoreInApcg(*theApcgType,
+              theMapType.core());
+          NS_ASSERT_MSG (theApcgCoreType.id() != "",
+              "Error: couldn't find core with ID " << theMapType.core() << " in the APCG XML!");
+          for (apcgType::core_type::task_const_iterator i(
+              theApcgCoreType.task().begin()); i
+              != theApcgCoreType.task().end(); i++)
+            {
+              apcgType::core_type::task_type theApcgTaskType = *i;
+              cerr << "\t\t task: " << theApcgTaskType;
+
+              string taskXmlFilePath = tasksFilePath + FILE_SEPARATOR + "task-"
+                  + theApcgTaskType + ".xml";
+
+              auto_ptr<task::taskType> theTaskType(task::task(taskXmlFilePath,
+                  flags::dont_validate));
+              NS_ASSERT_MSG (theApcgTaskType == theTaskType->ID(), "APCG XML says the task ID is " << theApcgTaskType
+                  << " but, task XML says the ID is " << theTaskType->ID() << "!");
+              cerr << " (name: " << theTaskType->name() << ")" << endl;
+
+              cerr << endl;
+            }
+        }
+
+      cerr << "Communications:" << endl;
+
+      for (ctgType::communication_const_iterator i(
+          theCtgType->communication().begin()); i
+          != theCtgType->communication().end(); i++)
+        {
+          ctgType::communication_type theCommunicationType = *i;
+
+          cerr << "\tsource task: " << theCommunicationType.source().id() << endl;
+          for (ctg::communicatingTaskType::deadline_const_iterator i(
+              theCommunicationType.source().deadline().begin()); i
+              != theCommunicationType.source().deadline().end(); i++)
+            {
+              ctg::communicatingTaskType::deadline_type deadline = *i;
+              cerr << "\t\tdeadline: " << deadline.type() << " at " << deadline << endl;
+            }
+
+          cerr << "\tdestination task: "
+              << theCommunicationType.destination().id() << endl;
+          for (ctg::communicatingTaskType::deadline_const_iterator i(
+              theCommunicationType.destination().deadline().begin()); i
+              != theCommunicationType.destination().deadline().end(); i++)
+            {
+              ctg::communicatingTaskType::deadline_type deadline = *i;
+              cerr << "\t\tdeadline: " << deadline.type() << " at " << deadline << endl;
+            }
+
+          cerr << "\tcommunication volume: " << theCommunicationType.volume()
+              << endl;
+
+          cerr << endl;
+        }
+
+    }
   catch (const xml_schema::exception& e)
-  {
-    cerr << e << endl;
-    return 1;
-  }
+    {
+      cerr << e << endl;
+      return 1;
+    }
 }
